3264cba18c4d81c7c2d825590687966826fae424
Ugur Akin <uakin@sfu.ca>
2018-11-02 02:01:16 -0700

    db-configuration added

--- ClubsWebApp/WebApp/config/database.php
+++ ClubsWebApp/WebApp/config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '10.142.0.2'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'SFU-Clubs-App-DB'),
+            'username' => env('DB_USERNAME', 'webhost'),
+            'password' => env('DB_PASSWORD', 'SFU-Clubs-App-1881'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
