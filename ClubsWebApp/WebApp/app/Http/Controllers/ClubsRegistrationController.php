c446bec52b3c75c30e918f5b995a7d19f40593e3
kunalgulati <kunalgulati98@gmail.com>
2018-11-04 17:08:22 -0800

    dependencies added for the registeration form

--- ClubsWebApp/WebApp/app/Http/Controllers/ClubsRegistrationController.php
+++ ClubsWebApp/WebApp/app/Http/Controllers/ClubsRegistrationController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 
 
 class ClubsRegistrationController extends Controller
