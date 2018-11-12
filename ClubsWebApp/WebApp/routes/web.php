564294f6d9b5555e520a767744b3ccd374f42f0a
kimmysj <zun710@hotmail.com>
2018-11-12 10:48:53 +0900

    Fixed welcome.blade.php to new version

--- ClubsWebApp/WebApp/routes/web.php
+++ ClubsWebApp/WebApp/routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/makeDummy','DummyController@makeDummy');
-Route::post('/dummy','DummyController@create');
 Route::get('/search','SearchController@index');
 Route::get('/clubCreate',function(){
     return view('clubCreate');
