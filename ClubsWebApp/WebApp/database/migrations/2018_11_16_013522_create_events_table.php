587a9d162590ead3130cdbd22dc5e0d1e58926d6
kunalgulati <kunalgulati98@gmail.com>
2018-11-15 17:41:20 -0800

    created migration to create a events table

---
+++ ClubsWebApp/WebApp/database/migrations/2018_11_16_013522_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('event_name');
+            $table->string('description');
+            $table->string('room');
+            $table->string('address');
+            $table->string('city');
+            $table->unsignedInteger('club_id');
+            $table->foreign('club_id')->references('id')->on('clubs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
