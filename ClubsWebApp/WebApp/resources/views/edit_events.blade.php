333db85fdc8c61ac09bee07e53967f2be6b4234f
kunalgulati <kunalgulati98@gmail.com>
2018-11-16 15:22:10 -0800

    margins changed

--- ClubsWebApp/WebApp/resources/views/edit_events.blade.php
+++ ClubsWebApp/WebApp/resources/views/edit_events.blade.php
@@ -4,33 +4,29 @@
 <!DOCTYPE>
 
 @foreach($events as $event)
-    <div class="card col-md-6 mt-5 mb-5" style='height: auto;'>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="100000" style='height: auto;'>
-            <div class="w-100 carousel-inner" role="listbox" >
-               <!-- Content Start-->
-                <div class="carousel-item active">
-                    <div class="carousel-caption">
-                        <div class="row">
-                            <!-- Image -->
-                            <div class="col-sm-3">
-                                <img src="https://img.icons8.com/color/96/000000/university.png" alt="" class="rounded-circle img-fluid" />
-                            </div>
-                            <div class="col-sm-9">
-                                <h3><b><label>Event Name: </label> {{$event['event_name']}}</h3> 
-                                <p><b><label>Description: </label></b> {{$event['description']}}</p>
-                                <p> <b><strong>Address: </strong></b> {{$event['room']}}, {{$event['address']}}, {{$event['city']}}</p>
-                                <p><label>Date: </label>TODO</p>
-                                <a href="delete_event/{{ $event->id }}" class="btn bg-success eventButton">Edit</a>
-                                <a href="#" class="btn bg-primary eventButton">Delete</a>
-
-                            </div>
+<div class="card col-md-6 mt-5 mb-5" style='height: auto;'>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="100000" style='height: auto;'>
+        <div class="w-100 carousel-inner" role="listbox" >
+            <div class="carousel-item active">
+                <div class="carousel-caption">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <img src="https://img.icons8.com/color/96/000000/university.png" alt="" class="rounded-circle img-fluid" />
+                        </div>
+                        <div class="col-sm-9">
+                            <h3><b><label>Event Name: </label> {{$event['event_name']}}</h3> 
+                            <p><b><label>Description: </label></b> {{$event['description']}}</p>
+                            <p> <b><strong>Address: </strong></b> {{$event['room']}}, {{$event['address']}}, {{$event['city']}}</p>
+                            <p><label>Date: </label>TODO</p>
+                            <a href="delete_event/{{ $event->id }}" class="btn bg-success eventButton">Edit</a>
+                            <a href="#" class="btn bg-primary eventButton">Delete</a>
                         </div>
                     </div>
                 </div>
-                <!-- Content End -->
             </div>
         </div>
     </div>
+</div>
 @endforeach
 
 
