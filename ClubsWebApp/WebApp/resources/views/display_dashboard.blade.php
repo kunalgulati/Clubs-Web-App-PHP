b871a594e81e8e8556a2280d62e7a99ab705d2df
uakin <ugur_akin@sfu.ca>
2018-11-19 18:59:42 -0800

    Remove head tag from display_dashboard.blade.php

--- ClubsWebApp/WebApp/resources/views/display_dashboard.blade.php
+++ ClubsWebApp/WebApp/resources/views/display_dashboard.blade.php
@@ -1,8 +1,3 @@
-<head> 
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
 @extends('main_layout')
 
 @section('content')
