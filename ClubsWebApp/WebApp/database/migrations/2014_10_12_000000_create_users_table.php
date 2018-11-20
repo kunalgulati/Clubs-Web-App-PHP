9e446902826a77ce0205b104574dff83b653d5b5
Ugur Akin <uakin@sfu.ca>
2018-11-20 04:01:50 -0800

    profile and user separated in db.

--- ClubsWebApp/WebApp/database/migrations/2014_10_12_000000_create_users_table.php
+++ ClubsWebApp/WebApp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
+            $table->string('sfu_email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
