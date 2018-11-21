0db02dff297378f6d7a2af6d38a8ef50fa4a879b
Ugur Akin <uakin@sfu.ca>
2018-11-20 16:47:27 -0800

    sex column fixed

--- ClubsWebApp/WebApp/database/migrations/2018_11_20_192122_create_profiles_table.php
+++ ClubsWebApp/WebApp/database/migrations/2018_11_20_192122_create_profiles_table.php
@@ -16,7 +16,7 @@ class CreateProfilesTable extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('gender');
+            $table->string('sex');
             $table->string('student_no');
             $table->string('major')->nullable();
             $table->string('year');
